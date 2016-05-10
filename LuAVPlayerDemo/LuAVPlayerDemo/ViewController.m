df99b7a2e186a500fad3f9a20d13c629db5a97a6
RamboLu <coderambolu@gmail.com>
2016-05-10 22:21:16 +0800

    Project initialization

---
+++ LuAVPlayerDemo/LuAVPlayerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LuAVPlayerDemo
+//
+//  Created by Louis on 16/5/10.
+//  Copyright © 2016年 RamboLu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
